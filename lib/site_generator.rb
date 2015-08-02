2a44766e20cc0566d9282722133ee8cb557bbd66
mbrickenworthe <adamwinters456@gmail.com>
2015-08-02 16:51:27 -0400

    Reworking final method

--- lib/site_generator.rb
+++ lib/site_generator.rb
@@ -13,28 +13,19 @@ class SiteGenerator
       @index += "<li><a href=\"movies/" + movie.url + "\">" + movie.title + "</a></li>" 
     end
     @index += "</ul></body></html>"
-    File.open('_site/index.html', 'w+') do |filey|
+    File.open('_site/index.html', 'a+') do |filey|
       filey.write(@index)
       filey.close
     end
   end
 
   def generate_pages!
-    @fullHash = Movie::SELF_NUMBER
-    Movie::SELF_NUMBER.each do |movie|
-      @title = movie.title
-      @release_date = movie.release_date
-      @director = movie.director
-      @summary = movie.summary
-      @fullHash
-      File.open('_site/movies/' + movie.url, 'w+') do |filey|
-        template = File.open('lib/templates/movie.html.erb', 'r') do |filey2|
-          template2 = File.read(filey2)
-          template3 = ERB.new template2 
-          filey.write(template3.result(binding))
-          filey.close
-          filey2.close
-        end
+  template = ERB.new(File.open('lib/templates/movie.html.erb').read)
+  movies = Movie.all
+    movies.each do |movie|
+    File.open("_site/movies/#{movie.url}", "w+") do |f|
+      f << template.result(binding)
+      f.close
       end
     end
   end
